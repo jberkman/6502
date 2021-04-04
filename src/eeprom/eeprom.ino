9a722dc39c9e4c303d02fe6653895f7718566e44
jacob berkman <jacob@87k.net>
2021-04-04 01:40:19 -0700

    reading works, writing doesn't.

--- src/eeprom/eeprom.ino
+++ src/eeprom/eeprom.ino
@@ -19,31 +19,134 @@
 
 */
 
-static const int addressPins[8] = { 10, 9, 8, 7, 6, 5, 4, 3 };
-static const int ioPins[8] = { 11, 12, 13, 2, A0, A1, A2, A3 };
+typedef const int BytePins[8];
+
+static BytePins addressPins = { 10, 9, 8, 7, 6, 5, 4, 3 };
+static BytePins ioPins = { 11, 12, 13, 2, A0, A1, A2, A3 };
 static const int writeEnable  = A5;
 static const int outputEnable = A4;
+static const byte resetHighAddr = 0xfd;
+static const byte resetLowAddr = 0xfc;
+
+enum Mode {
+    ReadMode,
+    WriteMode,
+    WriteResetVectorMode
+};
 
 static void
-digitalWriteByte(const int &pins[8], int value)
+digitalWriteByte (const BytePins &pins, byte value)
 {
+    // Serial.println(value, HEX);
     for (int i = 0; i < 8; ++i) {
-        digitalWrite(pins[i], value & (1 << i) ? HIGH : LOW);
+        // Serial.print(" ");
+        // Serial.print(i);
+        // Serial.print(" ");
+        // Serial.println(value & (1 << i) ? HIGH : LOW);
+        digitalWrite (pins[i], value & (1 << i) ? HIGH : LOW);
     }
+}
+
+static void
+digitalWriteByte (byte address, byte value)
+{
+    digitalWriteByte (addressPins, address);
+    digitalWriteByte (ioPins, value);
+    digitalWrite (writeEnable, LOW);
+    delayMicroseconds (1);
+    digitalWrite (writeEnable, HIGH);
+    delay (10);
+}
+
+static byte
+digitalReadByte (byte address)
+{
+    byte b = 0;
+    digitalWriteByte (addressPins, address);
+    for (int i = 0; i < 8; ++i) {
+        b |= digitalRead (ioPins[i]) << i;
+    }
+    return b;
+}
+
+static void
+dumpROM ()
+{
+    char line[81];
+    byte data[16];
+
+    digitalWrite (outputEnable, LOW);
+
+    for (int i = 0; i < 8; ++i) {
+        pinMode (ioPins[i], INPUT);
+    }
+
+    for (byte base = 0; ; base += sizeof(data)) {
+        for (byte i = 0; i < sizeof(data); ++i) {
+            data[i] = digitalReadByte(base + i);
+        }
+        snprintf(line, sizeof(line),
+                 "%08hhx: "
+                 "%02hhx%02hhx %02hhx%02hhx %02hhx%02hhx %02hhx%02hhx "
+                 "%02hhx%02hhx %02hhx%02hhx %02hhx%02hhx %02hhx%02hhx",
+                 base,
+                 data[0], data[1], data[2], data[3],
+                 data[4], data[5], data[6], data[7],
+                 data[8], data[9], data[10], data[11],
+                 data[12], data[13], data[14], data[15]);
+        Serial.println(line);
+        if (base == 240) {
+            return;
+        }
+    }
+}
+
+static void
+writePROM ()
+{
+    byte addr = 0;
+    do {
+        digitalWriteByte (addr, addr);
+        ++addr;
+    } while (addr);
+}
+
+static void
+writeResetVector ()
+{
+    digitalWriteByte (resetLowAddr, 0x00);
+    digitalWriteByte (resetHighAddr, 0x80);
 }
 
 void
 setup ()
 {
+    Mode mode = WriteMode;
+
+    digitalWrite (writeEnable, HIGH);
+    digitalWrite (outputEnable, HIGH);
+    
+    pinMode (writeEnable, OUTPUT);
+    pinMode (outputEnable, OUTPUT);
+
     for (int i = 0; i < 8; ++i) {
-        pinMode(addressPins[i], OUTPUT);
-        pinMode(ioPins[i], OUTPUT);
+        pinMode (addressPins[i], OUTPUT);
+        pinMode (ioPins[i], OUTPUT);
     }
-    pinMode(writeEnable, OUTPUT);
-    pinMode(outputEnable, OUTPUT);
+
+    Serial.begin (57600);
+
+    switch (mode) {
+        case WriteMode:
+            writePROM ();
+            break;
+
+        case WriteResetVectorMode:
+            writeResetVector ();
+            break;
+    }
+
+    dumpROM ();
 }
 
-void
-loop ()
-{
-}
+void loop () { }
