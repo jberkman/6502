0460c8a6b13dee25bb8ea8022e53deb47fd8c546
jacob berkman <jacob@87k.net>
2021-04-10 08:32:42 -0700

    Update working version

--- src/hello.s
+++ src/hello.s
@@ -7,75 +7,83 @@ E     = %10000000
 RW    = %01000000
 RS    = %00100000
 
-  .org $8000
+    .org $8000
+
 reset:
-  ldx #$ff
-  txs
-  
-  lda #%11111111 ; Set all pins on port B to output
-  sta DDRB
+    ldx #$ff
+    txs
 
-  lda #%11100000 ; Set top 3 pins on port A to output
-  STA DDRA
+    lda #%11111111 ; Set all pins on port B to output
+    sta DDRB
+    lda #%11100000 ; Set top 3 pins on port A to output
+    sta DDRA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
 
-  lda #%00111000 ; Set 8-bit mode, 2-line display, 5x8 font
-  jsr lcd_instruction
-  lda #%00001110 ; Display on, cursor on, blink off
-  jsr lcd_instruction
-  lda #%00000110 ; Increment and shift cursor but not display
-  jsr lcd_instruction
-  lda #%00000001 ; Clear display
-  jsr lcd_instruction
+    lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
+    jsr lcd_instruction
+    lda #%00001110 ; Display on; cursor on; blink off
+    jsr lcd_instruction
+    lda #%00000110 ; Increment and shift cursor; don't shift display
+    jsr lcd_instruction
+    lda #%00000001 ; Clear display
+    jsr lcd_instruction
 
-  lda #"H"
-  jsr print_char
-  lda #"e"
-  jsr print_char
-  lda #"l"
-  jsr print_char
-  lda #"l"
-  jsr print_char
-  lda #"o"
-  jsr print_char
-  lda #","
-  jsr print_char
-  lda #" "
-  jsr print_char
-  lda #"w"
-  jsr print_char
-  lda #"o"
-  jsr print_char
-  lda #"r"
-  jsr print_char
-  lda #"l"
-  jsr print_char
-  lda #"d"
-  jsr print_char
-  lda #"."
-  
+    ldx #0
+print:
+    lda message,x
+    beq loop
+    jsr print_char
+    inx
+    jmp print
+
 loop:
-  jmp loop
+    jmp loop
+
+message: .asciiz "Hello, world!"
+
+lcd_wait:
+    pha
+    lda #%00000000 ; Port B is input
+    sta DDRB
+lcd_busy:
+    lda #RW
+    sta PORTA
+    lda #(RW | E)
+    sta PORTA
+    lda PORTB
+    and #%10000000
+    bne lcd_busy
+
+    lda #0
+    sta PORTA
+    lda #%11111111 ; Port B is input
+    sta DDRB
+    pla
+    rts
 
 lcd_instruction:
-  sta PORTB
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
-  lda #E         ; Set E bit to send instruction
-  sta PORTA
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
-  rts
+    jsr lcd_wait
+    sta PORTB
+    lda #0
+    sta PORTA
+    lda #E         ; Set E bit to send instruction
+    sta PORTA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+    rts
 
 print_char:
-  STA PORTB
-  lda #RS        ; Set RS
-  sta PORTA
-  lda #(RS | E)  ; Set E bit to send instruction
-  STA PORTA
-  lda #RS         ; Set RS
-  sta PORTA
-  rts
+    jsr lcd_wait
+    sta PORTB
+    lda #RS
+    sta PORTA
+    lda #(RS | E)
+    sta PORTA
+    lda #0         ; Clear RS/RW/E bits
+    sta PORTA
+    rts
 
-  .org $fffc
-  .word reset
-  .word $0000
+    .org $fffc
+    .word reset
+    .word $0000
