822be6494ca42c4b7d764deb2937c55be6ae790a
jacob berkman <jacob@87k.net>
2021-04-10 08:31:23 -0700

    Cleanup

--- src/blink.s
+++ src/blink.s
@@ -4,6 +4,7 @@ DDRB  = $6002
 DDRA  = $6003
 
   .org $8000
+
 reset:
   lda #%11111111 ; Set all pins on port B to output
   sta DDRB
@@ -13,8 +14,8 @@ reset:
 
 loop:
   ror
-  sta $6000
-
+  sta PORTB
+  
   jmp loop
 
   .org $fffc
